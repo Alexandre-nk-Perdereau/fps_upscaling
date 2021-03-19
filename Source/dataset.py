04ed73ec0617309cdc8d94979c2c6244d32b0631
AlexandrePerdereau <43601699+AlexandrePerdereau@users.noreply.github.com>
2021-03-19 10:04:53 +0100

    dataset_reading

---
+++ Source/dataset.py
@@ -0,0 +1,53 @@
+from torch.utils.data import Dataset
+from config import datasets_path
+from os.path import join
+from PIL import Image
+import torchvision.transforms.transforms as transforms
+from config import debug_path
+
+TRAINING_SET = 0
+TEST_SET = 1
+
+
+class Frameupscaling_Dataset(Dataset):
+
+    def __init__(self, folder_list, set):
+        self.triplets = []
+
+        if set ==TRAINING_SET:
+            self.transformations = transforms.Compose([transforms.ToTensor()])
+        else:
+            self.transformations = transforms.Compose([transforms.ToTensor()])
+
+        for folder in folder_list:
+            triplets_folder = open(join(datasets_path + folder, "triplets.txt"), 'r')
+            triplet_strings = (triplets_folder.read()).split(';')
+            for triplet_string in triplet_strings:
+                triplet = triplet_string.split(',')
+                for i in range(3):
+                    triplet[i] = join(datasets_path + folder, triplet[i])
+                self.triplets.append(triplet)
+
+    def __getitem__(self, i):
+        triplet = self.triplets[i]
+
+        previous_image = Image.open(triplet[0])
+        following_image = Image.open(triplet[2])
+        target_image = Image.open(triplet[1])
+
+        previous_tensor = self.transformations(previous_image)
+        following_tensor = self.transformations(following_image)
+        target_tensor = self.transformations(target_image)
+        # I will need to change it when I will add random transforms to apply the same transforms to the triplet
+
+        return previous_tensor, following_tensor, target_tensor
+
+    def __len__(self):
+        return len(self.triplets)
+
+
+if __name__ == "__main__":
+    test_ds = Frameupscaling_Dataset(['test_ds'], TRAINING_SET)
+    test_tensor, _, _ = test_ds[1000]
+    test_image = transforms.ToPILImage()(test_tensor)
+    test_image.save(join(debug_path, "test_image.jpg"))
--- Source/dataset_creation.py
+++ Source/dataset_creation.py
@@ -1,23 +1,40 @@
-from random import random
-
 import cv2
 import os
 
 
 def video_to_frames(video_path, path_output_dir):
+
+    current_triplet = []
     vid_cap = cv2.VideoCapture(video_path)
     count = 0
+    file_triplets = open(os.path.join(path_output_dir, 'triplets.txt'), 'w')
+
     while vid_cap.isOpened():
         success, image = vid_cap.read()
         if success:
-            cv2.imwrite(os.path.join(path_output_dir, 'frame%d.png') % count, image)
+            cv2.imwrite(os.path.join(path_output_dir, 'frame_%d.png') % count, image)
+            if len(current_triplet) == 3:
+                current_triplet[0] = current_triplet[1]
+                current_triplet[1] = current_triplet[2]
+                current_triplet[2] = "frame_" + str(count) + ".png"
+                if count > 3:
+                    string_triplets = ";" + current_triplet[0] + "," + current_triplet[1] + "," + current_triplet[2]
+                else:
+                    string_triplets = current_triplet[0] + "," + current_triplet[1] + "," + current_triplet[2]
+                file_triplets.write(string_triplets)
+            else:
+                current_triplet.append("frame_" + str(count) + ".png")
             count += 1
+            if count % 100 == 0:
+                print(count)
         else:
             break
     cv2.destroyAllWindows()
     vid_cap.release()
+    file_triplets.close()
+
 
 
 if __name__ == '__main__':
-    video_to_frames(r"D:\Star wars for Super Resolution\TheMandalorian_S2E1_1080pH264.mkv",
-                    r"D:\github_local\fps_upscaling\Data\Datasets\TM_S2E1")
+    video_to_frames(r"D:\blender_videos\test.mkv",
+                    r"D:\github_local\fps_upscaling\Data\Datasets\test_ds")
